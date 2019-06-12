9a92d3609321871ea35ee214f8dbbf95971947fe
Shawn <slsj1997@gmail.com>
2019-06-12 17:46:58 +0800

    Added User Onboarding (incomplete)

---
+++ Android App/app/src/main/java/com/marshmallow/paywhere/Onboarding.java
@@ -0,0 +1,71 @@
+package com.marshmallow.paywhere;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Onboarding extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        mSlideViewPager = findViewById(R.id.slideViewPager);
+        mDotLayout = findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(this);
+
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+    }
+
+    public void addDotsIndicator(int position) {
+        mDots = new TextView[sliderAdapter.getCount()];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < mDots.length; i++) {
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getColor(R.color.statusBar));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0) {
+            mDots[position].setTextColor(getColor(R.color.colorPrimary));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener= new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+}
---
+++ Android App/app/src/main/java/com/marshmallow/paywhere/SliderAdapter.java
@@ -0,0 +1,75 @@
+package com.marshmallow.paywhere;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    public int[] slide_images = {
+            R.drawable.app_icon,
+            R.drawable.pin_drop,
+            R.drawable.filter_tool,
+            R.drawable.thumbs_up
+    };
+
+    public String[] slide_headings = {
+            "Thank you for choosing PayWhere!",
+            "Choose a shopping mall.",
+            "Filter by your preferred mobile payment option.",
+            "Enjoy your meal along with easy payment!"
+    };
+
+    public String[] slide_desc = {
+            "Swipe to learn how to use our app.",
+            "Swipe to learn how to use our app.",
+            "Swipe to learn how to use our app.",
+            "Swipe to learn how to use our app."
+    };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = view.findViewById(R.id.slideImageView);
+        TextView slideHeading = view.findViewById(R.id.slideHeading);
+        TextView slideDesc = view.findViewById(R.id.slideDesc);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDesc.setText(slide_desc[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ConstraintLayout)object);
+    }
+}
