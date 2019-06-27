ab97561134702da564da78a75e977681275646cc
Shawn <slsj1997@gmail.com>
2019-06-27 14:21:32 +0800

    Added Error Page When No Search Result Found

---
+++ Android App/app/src/main/java/com/marshmallow/paywhere/ErrorResults.java
@@ -0,0 +1,46 @@
+package com.marshmallow.paywhere;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ErrorResults extends AppCompatActivity {
+
+    private Toolbar toolBar;
+    private SearchView searchView;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_results);
+        toolBar = findViewById(R.id.errorToolbar);
+        setSupportActionBar(toolBar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowTitleEnabled(false);
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent searchActivity = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(searchActivity);
+                finish();
+            }
+        });
+
+//        searchView = findViewById(R.id.errorSearchView);
+//        Bundle bundle = getIntent().getExtras();
+//        String input = bundle.getString("input");
+//        searchView.setQuery(input, false);
+
+        textView = findViewById(R.id.errorTextView);
+        String errorMsg1 = getResources().getString(R.string.error_text_1);
+        String errorMsg2 = getResources().getString(R.string.error_text_2);
+        textView.setText(errorMsg1 + '\n' + errorMsg2);
+    }
+}
