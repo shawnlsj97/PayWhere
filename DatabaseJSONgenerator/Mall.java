e263a7e58925e1af04b52a83c754b6f88340567e
Dominique Ng <dominiqueng@me.com>
2019-06-28 15:54:17 +0800

    Added class to handle multiple mall inputs

---
+++ DatabaseJSONgenerator/Mall.java
@@ -0,0 +1,18 @@
+class Mall {
+    private String name;
+    public Restaurant[] restArray;
+    public static Mall[] mallArray;
+
+    Mall(String name, int numRest) {
+        this.name = name;
+        this.restArray = new Restaurant[numRest];
+    }
+
+    public static void printMallClose() {
+        System.out.println("    },");
+    }
+
+    public static void printMallCloseLast() {
+        System.out.println("    }");
+    }
+}
