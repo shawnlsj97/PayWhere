654ccbd99dcd80e2579f7b13b16e91da3fbe0247
Dominique Ng <dominiqueng@me.com>
2019-06-30 20:08:09 +0800

    Will add Dash in payment key if applicable

--- DatabaseJSONgenerator/Restaurant.java
+++ DatabaseJSONgenerator/Restaurant.java
@@ -7,19 +7,37 @@ class Restaurant {
         this.unitNumber = unitNumber;
     }
 
+    public boolean hasDash() {
+        Boolean containsFlag = false;
+        for (String merchant : Main.dashArray) {
+            if (name.compareToIgnoreCase(merchant) == 0) {
+                containsFlag = true;
+            }
+        }
+        return containsFlag;
+    }
+
     public void printRest() {
         System.out.println("        \"" + name + "\" : {");
-        System.out.println("            \"Name\" : " + "\"" + name + "\",");
-        System.out.println("            \"Unit Number\" : " + "\"" + unitNumber+ "\",");
-        System.out.println("            \"Payment\" : " + "\"" + "\"");
+        System.out.println("            \"name\" : " + "\"" + name + "\",");
+        System.out.println("            \"address\" : " + "\"" + unitNumber+ "\",");
+        if (hasDash()) {
+            System.out.println("            \"payment\" : " + "\"Dash\"");
+        } else {
+            System.out.println("            \"payment\" : " + "\"" + "\"");
+        }
         System.out.println("        " + "},");
     }
-    
+
     public void printRestLast() {
         System.out.println("        \"" + name + "\" : {");
-        System.out.println("            \"Name\" : " + "\"" + name + "\",");
-        System.out.println("            \"Unit Number\" : " + "\"" + unitNumber + "\",");
-        System.out.println("            \"Payment\" : " + "\"" + "\"");
+        System.out.println("            \"name\" : " + "\"" + name + "\",");
+        System.out.println("            \"address\" : " + "\"" + unitNumber + "\",");
+        if (hasDash()) {
+            System.out.println("            \"payment\" : " + "\"Dash\"");
+        } else {
+            System.out.println("            \"payment\" : " + "\"" + "\"");
+        }
         System.out.println("        " + "}");
     }
 }
