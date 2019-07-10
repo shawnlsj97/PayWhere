8ee1d8dd562ca05a55e1412985c783f69bfe107c
Dominique Ng <dominiqueng@me.com>
2019-07-10 16:09:44 +0800

    Removed SortMall folder

--- SortMall/Mall.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Mall {
-    String name;
-
-    Mall(String name) {
-        this.name = name;
-    }
-}
--- SortMall/MallComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class MallComparator implements Comparator<Mall> {
-    @Override
-    public int compare(Mall a, Mall b) {
-        return a.name.compareToIgnoreCase(b.name);
-    }
-}
