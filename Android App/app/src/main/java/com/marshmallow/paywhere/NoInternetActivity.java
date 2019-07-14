5f0ff53d835727f7c83031d1fa031db47577461c
Shawn <slsj1997@gmail.com>
2019-07-14 17:31:42 +0800

    Added automatic detection for changes in internet connection

--- Android App/app/src/main/java/com/marshmallow/paywhere/NoInternetActivity.java
+++ Android App/app/src/main/java/com/marshmallow/paywhere/NoInternetActivity.java
@@ -6,20 +6,28 @@ import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.ContentLoadingProgressBar;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NoInternetActivity extends AppCompatActivity {
 
     private TextView retry;
     private Toolbar toolBar;
     private ContentLoadingProgressBar pb;
+    private ConnectivityManager cm;
+    private BroadcastReceiver networkChangeReceiver;
+    private boolean registered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,13 +83,43 @@ public class NoInternetActivity extends AppCompatActivity {
                 }
             }
         });
+
+        networkChangeReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                boolean isConnected = false;
+                if (connectivity != null) {
+                    NetworkInfo[] info = connectivity.getAllNetworkInfo();
+                    if (info != null) {
+                        for (int i = 0; i < info.length; i++) {
+                            if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                                if (!isConnected) {
+                                    isConnected = true;
+                                    Intent searchActivity = new Intent(getApplicationContext(), SearchActivity.class);
+                                    startActivity(searchActivity);
+                                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                                    finish();
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    isConnected = false;
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
+        registered = true;
     }
 
     private boolean haveNetworkConnection() {
         boolean haveConnectedWifi = false;
         boolean haveConnectedMobile = false;
 
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo[] netInfo = cm.getAllNetworkInfo();
         for (NetworkInfo ni : netInfo) {
             if (ni.getTypeName().equalsIgnoreCase("WIFI"))
@@ -98,5 +136,53 @@ public class NoInternetActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        if (registered) {
+            unregisterReceiver(networkChangeReceiver);
+            registered = false;
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
+        registered = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
+        registered = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (registered) {
+            unregisterReceiver(networkChangeReceiver);
+            registered = false;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (registered) {
+            unregisterReceiver(networkChangeReceiver);
+            registered = false;
+        }
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        if (registered) {
+            unregisterReceiver(networkChangeReceiver);
+            registered = false;
+        }
     }
 }
